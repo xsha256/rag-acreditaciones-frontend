78685b3dbd21e92656e7f45007cff31e989137a3
Óscar Esteve Aleixandre <165893605+oscaresteve@users.noreply.github.com>
2026-03-12 12:52:08 +0100

    Feedback/estilos (#52)
    * añadir ruta para el panel de calidad en feedback
    
    * modificar los estilos del componente para usar el tema de styles.css

--- src/app/features/feedback/feedback.routes.ts
+++ src/app/features/feedback/feedback.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
+import { CalidadPanelComponent } from './calidad/ui/calidad-panel-component/calidad-panel-component';
 
 // El equipo de Feedback importará sus componentes aquí:
 
-
 export const FEEDBACK_ROUTES: Routes = [
-
+  { path: 'panel-calidad', component: CalidadPanelComponent },
 ];
